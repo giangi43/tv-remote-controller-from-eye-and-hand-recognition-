f807b0f6b8681925a06befa958b4036ee756739f
giangi43 <giangi.mamusa@gmail.com>
2014-12-08 22:01:57 +0100

    ma come funziona
    non capisco come funzioni github

---
+++ HandGestureRecognition/HandGestureRecognition/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace HandGestureRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBoxFrameGrabber = new Emgu.CV.UI.ImageBox();
+            this.splitContainerFrames = new System.Windows.Forms.SplitContainer();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBoxSkin = new Emgu.CV.UI.ImageBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxFrameGrabber)).BeginInit();
+            this.splitContainerFrames.Panel1.SuspendLayout();
+            this.splitContainerFrames.Panel2.SuspendLayout();
+            this.splitContainerFrames.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxSkin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBoxFrameGrabber
+            // 
+            this.imageBoxFrameGrabber.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBoxFrameGrabber.Location = new System.Drawing.Point(0, 0);
+            this.imageBoxFrameGrabber.Margin = new System.Windows.Forms.Padding(4);
+            this.imageBoxFrameGrabber.Name = "imageBoxFrameGrabber";
+            this.imageBoxFrameGrabber.Size = new System.Drawing.Size(431, 481);
+            this.imageBoxFrameGrabber.TabIndex = 2;
+            this.imageBoxFrameGrabber.TabStop = false;
+            this.imageBoxFrameGrabber.Click += new System.EventHandler(this.imageBoxFrameGrabber_Click);
+            // 
+            // splitContainerFrames
+            // 
+            this.splitContainerFrames.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerFrames.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerFrames.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainerFrames.Name = "splitContainerFrames";
+            // 
+            // splitContainerFrames.Panel1
+            // 
+            this.splitContainerFrames.Panel1.Controls.Add(this.label5);
+            this.splitContainerFrames.Panel1.Controls.Add(this.comboBox2);
+            this.splitContainerFrames.Panel1.Controls.Add(this.checkBox1);
+            this.splitContainerFrames.Panel1.Controls.Add(this.comboBox1);
+            this.splitContainerFrames.Panel1.Controls.Add(this.label4);
+            this.splitContainerFrames.Panel1.Controls.Add(this.label3);
+            this.splitContainerFrames.Panel1.Controls.Add(this.label2);
+            this.splitContainerFrames.Panel1.Controls.Add(this.label1);
+            this.splitContainerFrames.Panel1.Controls.Add(this.button1);
+            this.splitContainerFrames.Panel1.Controls.Add(this.imageBoxFrameGrabber);
+            // 
+            // splitContainerFrames.Panel2
+            // 
+            this.splitContainerFrames.Panel2.Controls.Add(this.imageBoxSkin);
+            this.splitContainerFrames.Size = new System.Drawing.Size(913, 481);
+            this.splitContainerFrames.SplitterDistance = 431;
+            this.splitContainerFrames.SplitterWidth = 5;
+            this.splitContainerFrames.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "label5";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(3, 115);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(38, 24);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(388, 48);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "COM4",
+            "COM5",
+            "COM10",
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM Andante"});
+            this.comboBox1.Location = new System.Drawing.Point(388, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(117, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(180, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "salva";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBoxSkin
+            // 
+            this.imageBoxSkin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBoxSkin.Location = new System.Drawing.Point(0, 0);
+            this.imageBoxSkin.Margin = new System.Windows.Forms.Padding(4);
+            this.imageBoxSkin.Name = "imageBoxSkin";
+            this.imageBoxSkin.Size = new System.Drawing.Size(477, 481);
+            this.imageBoxSkin.TabIndex = 2;
+            this.imageBoxSkin.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(913, 481);
+            this.Controls.Add(this.splitContainerFrames);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxFrameGrabber)).EndInit();
+            this.splitContainerFrames.Panel1.ResumeLayout(false);
+            this.splitContainerFrames.Panel1.PerformLayout();
+            this.splitContainerFrames.Panel2.ResumeLayout(false);
+            this.splitContainerFrames.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxSkin)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBoxFrameGrabber;
+        private System.Windows.Forms.SplitContainer splitContainerFrames;
+        private Emgu.CV.UI.ImageBox imageBoxSkin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
